f1c5c77ef715fe1ee5e7443e6365bffce179885a
Guanyu Hu <ikaroinory@gmail.com>
2025-05-09 20:08:52 +0800

    feat: Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch_geometric
+
+conda install loguru -y
+conda install pandas -y
+conda install scikit-learn -y
+
+pip install optuna optuna-dashboard
