2332fd4cf0669fab71da5578d8f65c9c2a246090
Guanyu Hu <ikaroinory@gmail.com>
2025-05-24 23:07:32 +0800

    fix

--- utils/OptunaArguments.py
+++ utils/OptunaArguments.py
@@ -17,7 +17,7 @@ class OptunaArguments:
         self.device = 'cuda'
 
         self.batch_size: int = trial.suggest_categorical('batch_size', [32, 64, 128])  # best: 256
-        self.epochs: int = 100
+        self.epochs: int = 150
 
         self.slide_window: int = trial.suggest_int('slide_window', 5, 50)
         self.slide_stride: int = 1
