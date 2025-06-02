99a41ab93d8dc68f896ba1c3b7b7c1545e41ab7c
Guanyu Hu <ikaroinory@gmail.com>
2025-06-02 19:15:13 +0800

    g

--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install torch_geometric
-
-conda install loguru -y
-conda install pandas -y
-conda install scikit-learn -y
-
-pip install optuna optuna-dashboard
-
 # New
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 pip install torch_geometric
